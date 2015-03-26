3d87488c5680f653e2f2177d54dac931bb96a910
jd3johns <jd3johns@uwaterloo.ca>
2015-03-25 21:49:35 -0400

    Update water drop agent class

--- code/WaterDrop.m
+++ code/WaterDrop.m
@@ -9,8 +9,7 @@ classdef WaterDrop % < handle % inherit from handle to allow pass by reference?
    % public properties
       soil = 0;
       vel = 0;
-      node = 0;
-      node_prev = 0;
+      route = [];
    end
    
    properties(GetAccess = 'public', SetAccess = 'private')
@@ -20,19 +19,20 @@ classdef WaterDrop % < handle % inherit from handle to allow pass by reference?
       b_v = 0;
       c_v = 0;
       
-      depot = 0;
       capacity = 0;
       
-      epsilon = 0.001
+      epsilon = 0.001;
+      epsilon_s = 0.01;
    end
    
    methods
    % public by default
    
-      function obj = WaterDrop(depot, capacity, a_v, b_v, c_v)
+      function obj = WaterDrop(depot, capacity, velocity, a_v, b_v, c_v)
       % class constructor
-         obj.depot = depot;
+         obj.route = [obj.route; depot];
          obj.capacity = capacity;
+         obj.vel = velocity;
          
          % velocity params
          obj.a_v = a_v;
@@ -40,15 +40,72 @@ classdef WaterDrop % < handle % inherit from handle to allow pass by reference?
          obj.c_v = c_v;
       end
 
-      function obj = updateLocation(obj, node)
-         obj.node_prev = obj.node;
-         obj.node = node;
-      end
-      function obj = updateSoil(obj, edge)
+      function obj = updateSoil(obj, soil_mat)
          obj.soil = obj.soil + 0.1;%edge.soil(node, node_prev);
       end
-      function obj = updateVelocity(obj, edge)
-         obj.vel = obj.vel + a_v/(b_v + c_v*0.01);%edge.soil(node, node_prev));
+
+      function obj = updateVelocity(obj, soil_mat)
+         n_idx = length(obj.route);
+         obj.vel = obj.vel + a_v/(b_v + c_v*soil_mat(route(n_idx), route(n_idx - 1)));
+      end
+
+      function obj = flow(obj, soil_mat, customers) % custs that haven't been visited
+         probs = pathProbs(soil_mat, customers);
+         node = selectPath(probs);
+
+         obj.route = [obj.route; node];
+      end
+   end
+   
+   methods(Access = 'private')
+   % private methods
+      
+      function path = selectPath(obj, probs)
+      % select a path from probabilities of possible edges
+         r = rand();
+         [sorted idx] = sort(probs,'descend');
+         runsum = 0;
+         for i = 1:length(sorted)
+            runsum = runsum + sorted(i);
+            if r < runsum
+               path = idx(i);
+            end
+         end
+      end
+      
+      function probs = pathProbs(obj, soil_mat, customers)
+      % calculate the probabilities of traversing possible paths
+         invsoils = zeros(length(customers),1);
+         for i = 1:length(customers)
+            invsoils(i) = invSoil(customers(i), soil_mat, customers);
+         end
+         sumsoils = sum(invsoils);
+         
+         probs = zeros(length(customers,1));
+         for i = 1:length(customers)
+            f = invSoil(customers(i), soil_mat, customers);
+            probs(i) = f/sumsoils;
+         end
+      end
+
+      function f = invSoil(obj, customer, soil_mat, customers)
+      % inverse term calculation
+         f = 1/(epsilon_s + positiveSoil(customer, soil_mat, customers));
+      end
+
+      function g = positiveSoil(obj, customer, soil_mat, customers)
+      % term calculation
+         soils = zeros(length(customers),1);
+         for i = 1:length(customers)
+            soils(i) = soil_mat(customer,customers(i));
+         end
+         min_s = min(soils);
+         
+         if min_s >= 0
+            g = soils(customer);
+         else
+            g = soils(customer) - min_s;
+         end
       end
    end
 end
